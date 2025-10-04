9af1ae04b066d64dd96eee876691eb5708cff337
Azri Sols <ishrafazri@gmail.com>
2025-10-04 11:24:23 +0800

    FIX: Header img background not clear

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,7 @@ export default function Header() {
         <div
           className="absolute inset-0 bg-cover bg-center bg-fixed opacity-40"
           style={{
-            backgroundImage:
-              "url('https://images.unsplash.com/photo-1596040033229-a9821ebd058d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')",
+            backgroundImage: "url('/images/header-bg.jpeg')",
           }}
         ></div>
         {/* Decorative Elements */}
